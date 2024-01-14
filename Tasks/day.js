d4d0c736a799e338f3fcf8227dacdda241b9303a
vegetable5 <143118983+vegetable5@users.noreply.github.com>
2024-01-14 20:12:15 +0200

    added 'use strict', fixed variable/function declaration

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,10 +1,9 @@
 // Get day number
+'use strict';
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+const _parse_day_ = (s) => {
+  for (let i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
