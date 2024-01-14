99e1856e89362dcf676e42e86ee1e040527a4bee
vegetable5 <143118983+vegetable5@users.noreply.github.com>
2024-01-14 21:14:00 +0200

    replaced forEach() with for..of

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,10 +4,10 @@
 const objectSum = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+  };
   return sum;
 };
 
