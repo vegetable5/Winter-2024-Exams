5f90cabedba3424ed6e61534f9ab8b82eb5c3a5e
vegetable5 <143118983+vegetable5@users.noreply.github.com>
2024-01-14 20:17:11 +0200

    fixed naming

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,14 @@
 // Get day number
 'use strict';
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const _parse_day_ = (s) => {
-  for (let i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+const parseDay = (day) => {
+  for (let i = 0; i < days.length; i++) {
+    if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
