84bdbdd51e88e92ed99553550d199cd0f962f05b
vegetable5 <143118983+vegetable5@users.noreply.github.com>
2024-01-14 20:47:21 +0200

    replace forEach with for..of

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,14 +2,13 @@
 'use strict';
 
 const reverseDictionary = (data) => {
-  const keys = Object.keys(data, 500);
-  ({ ...data });
-  keys.forEach((_) => {
-    const key = data[_];
-    data[key] = _;
-    delete data[_];
-  }, 1000);
-  return data;
+  const keys = Object.keys(data);
+  const reversed = {};
+  for (const key of keys) {
+    const index = data[key];
+    reversed[index] = key;
+  }
+  return reversed;
 };
 
 module.exports = reverseDictionary;
